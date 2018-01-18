c067fbd0d5d9c5f4e96a153b74122547bf122a25
Allen Yunseok Lee <pineoc@naver.com>
2018-01-18 18:02:42 +0900

    add sample key.js file

---
+++ routes/key.js
@@ -0,0 +1,15 @@
+var key_data = {
+	android_key: "api-key string",
+	ios_key_option: {
+        gateway : 	"gateway.sandbox.push.apple.com",
+        cert: 		'../../keys/cert.pem',
+        key: 		'../../keys/key.pem'
+    },
+    android_device_key: [
+    	"device registrationId"
+    ],
+    ios_device_key: [
+    	""
+    ]
+}
+exports.key_data = key_data;
